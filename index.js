87b2aed53130982ca7d98ccca11dc78409ab4218
brian ambundo <brian.katiechi9@gmail.com>
2022-06-17 12:21:30 +0300

    commit tree 863be8b61029448545335cdd52e81a2343d03b4e

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+const neno = word.toLowerCase()
+const  splitWord = neno.split('')
+  const reverseWord = splitWord.reverse()
+  reverseJoined = reverseWord.join("")
+  if(neno == reverseJoined){
+    return true
+  }
+  else{
+    return false
+  }
 
+ 
+}
+isPalindrome('Madam')
 /* 
   Add your pseudocode here
+  Input the parameter into the function
+  parameter.toLowerCase
+  The output is split into an array of letters
+  reverse the array and join it back into a word
+  use the if else statement to check whether the parameter and joined word match
+  return true or false
 */
 
 /*
   Add written explanation of your solution here
+  Take the input and convert it to lowercase then make it into an array, reverse the letters 
+  in the array and join them back together, check whether the name written foward is the same as the
+  name written backwards
 */
 
-// You can run `node index.js` to view these console logs
+ //You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  //add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
